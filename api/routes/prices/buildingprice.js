4c1187fcc0c39d8a2af262e4ab68b90550f2cf8d
ZigLost <lim.ratchata@gmail.com>
2017-04-11 17:50:46 +0700

    Add number formatting

--- api/routes/prices/buildingprice.js
+++ api/routes/prices/buildingprice.js
@@ -3,18 +3,18 @@ const EpsUtil = require('../../../models/util');
 const Property = require('../../../models/Property');
 
 // To get Building Price based on province and property type
-// (accessed at GET http://localhost:8280/api/prices/:province/:proptype/:unitamount)
+// (accessed at GET http://localhost:8280/api/prices/building/:province/:proptype/:unitamount)
 module.exports = (req, res) => {
     Property.find({
         province: req.params.province,
         propertyType: req.params.proptype
     }, 'propertyType province price', (err, props) => {
-        if(err) return res.send(res);
+        if(err) return res.send(err);
         else if(props.length === 0) return res.status(404).send('Property not found');
         
         let util = new EpsUtil();
         for(let p of props) {
-            p.price = p.price * util.ConvertToSquareMeter(req.params.unitamount);
+            p.price = (p.price * util.ConvertToSquareMeter(req.params.unitamount)).toLocaleString();
         }
         res.json(props);
     });
