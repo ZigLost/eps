01b19943a55c56db0d5b5884a3ef76e95fd4288e
ZigLost <lim.ratchata@gmail.com>
2017-04-11 11:59:34 +0700

    Add more model

---
+++ models/Land.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const LandSchema = new mongoose.Schema({
+    province: { type: String, required: true },
+    district: { type: String, required: true },
+    road: { type: String, required: true },
+    minPrice: Number,
+    maxPrice: Number
+});
+LandSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('Land', LandSchema);
