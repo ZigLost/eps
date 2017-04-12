cc9f18cbd21564bd7f0dcd09f41a8d6bc7080524
ZigLost <lim.ratchata@gmail.com>
2017-04-12 17:34:57 +0700

    Modify to import multiple data source files at once

--- db/parseLandPrice.js
+++ db/parseLandPrice.js
@@ -5,46 +5,61 @@ const path = require('path');
 const db = require('../api/db');
 const Land = require('../models/Land');
 
-let importedFile = './sourcefiles/นนทบุรี.txt',
-    errorFile = './error.json',
-    outFile = './นนทบุรี.json';
-let lands = [], district = '', province = path.basename(importedFile, '.txt');
+let sourcefilesDir = './sourcefiles',
+    errorFile = './error.json';
+let lands = [], district = '';
 
 if(fs.existsSync(errorFile)) fs.unlink(errorFile);
 
 let eachLine = Promise.promisify(lineReader.eachLine);
-eachLine(importedFile, (line) => {
-    let land = {};
-    // fix bug first line contains ﻿ (line-reader)
-    if(line == '﻿') return;
-    if(line.includes('อำเภอ')) {
-        district = line;
-        return;
-    }
 
-    land.province = province;
-    land.district = district;
-    let price = [], i = line.length - 1;
-    for(; i >= 0; i--) {
-        if(line.charAt(i) === ',') continue;
-        if(line.charAt(i) === ' ') break;
-        price.unshift(line.charAt(i));
-    }
-    land.road = line.substring(0, i);
+fs.readdir(sourcefilesDir, (err, files) => {
+    files.filter((f) => { 
+        return f.includes('.txt');
+    }).forEach((file) => {
+        file = `${sourcefilesDir}/${file}`;
+        let province = path.basename(file, '.txt');
 
-    let realPrices = price.join('').split('-');
-    land.minPrice = parseFloat(realPrices[0]);
-    land.maxPrice = parseFloat(realPrices.length > 1 ? realPrices[1] : realPrices[0]);
+        Land.remove({}, (err) => {
+            if(err) throw err;
 
-    if(land.maxPrice < land.minPrice) {
-        fs.appendFileSync(errorFile, JSON.stringify(land) + '\n');
-    }
-    
-    lands.push(land);
-}).then((err) => {
-    if(err) throw err;
-    //fs.writeFile(outFile, JSON.stringify(lands));
-    Land.insertMany(lands, (err, props) => {
-        if(err) console.log(err);
+            eachLine(file, (line) => {
+                let land = new Land();
+                // fix bug first line contains ﻿ (line-reader)
+                if(line == '﻿') return;
+                if(line.includes('อำเภอ')) {
+                    district = line;
+                    return;
+                }
+
+                land.province = province;
+                land.district = district;
+                let price = [], i = line.length - 1;
+                for(; i >= 0; i--) {
+                    if(line.charAt(i) === ',') continue;
+                    if(line.charAt(i) === ' ') break;
+                    price.unshift(line.charAt(i));
+                }
+                land.road = line.substring(0, i);
+
+                let realPrices = price.join('').split('-');
+                land.minPrice = parseFloat(realPrices[0]);
+                land.maxPrice = parseFloat(realPrices.length > 1 ? realPrices[1] : realPrices[0]);
+
+                if(land.maxPrice < land.minPrice) {
+                    fs.appendFileSync(errorFile, JSON.stringify(land) + '\n');
+                }
+                
+                lands.push(land);
+            }).then((err) => {
+                if(err) throw err;
+
+                console.log(`InsertMany ${province}`);
+                Land.insertMany(lands, (err, props) => {
+                    if(err) console.log(err);
+                    console.log(`Completed import data ${province}`);
+                });
+            });
+        });
     });
 });
